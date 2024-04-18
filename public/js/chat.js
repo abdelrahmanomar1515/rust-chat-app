1ab7ca7cd65c970005c873dbb6673e5eb99d81df
Abdurrahman Abdulaziz <abdelrahman_omar_1515@yahoo.com>
2024-04-18 20:39:12 +0200

    simplify by using tokio broadcast channel

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,63 +1,53 @@
-const socket = new WebSocket("ws://127.0.0.1:8080/ws");
+const url = new URL("ws://127.0.0.1:8080/ws");
+url.search = new URLSearchParams(window.location.search);
+const socket = new WebSocket(url);
 
 function scrollToBottom() {
-    const messageList = jQuery("#message-list");
-    const newMessage = messageList.children("li:last-child");
+  const messageList = jQuery("#message-list");
+  const newMessage = messageList.children("li:last-child");
 
-    const clientHeight = messageList.prop("clientHeight");
-    const scrollTop = messageList.prop("scrollTop");
-    const scrollHeight = messageList.prop("scrollHeight");
-    const newMessageHeight = newMessage.innerHeight();
-    const lastMessageHeight = newMessage.prev().innerHeight();
+  const clientHeight = messageList.prop("clientHeight");
+  const scrollTop = messageList.prop("scrollTop");
+  const scrollHeight = messageList.prop("scrollHeight");
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
 
-    if (
-        clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
-        scrollHeight
-    ) {
-        messageList.scrollTop(scrollHeight);
-    }
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
+    messageList.scrollTop(scrollHeight);
+  }
 }
 
-socket.addEventListener("open", function() {
-    const params = jQuery.deparam(window.location.search);
-
-    socket.send(JSON.stringify(params));
-});
-
-socket.addEventListener("close", function() {
-    console.log("disconnected");
+socket.addEventListener("message", function(msg) {
+  const message = JSON.parse(msg.data);
+  if (message.type === "updateUserList") {
+    let ol = jQuery("<ol></ol>");
+    for (let user of msg) {
+      ol.append(jQuery("<li></li>").text(user));
+    }
+    jQuery("#users").html(ol);
+  }
 });
 
 socket.addEventListener("message", function(msg) {
-    const message = JSON.parse(msg.data);
-    if (message.type === "updateUserList") {
-        let ol = jQuery("<ol></ol>");
-        for (let user of msg) {
-            ol.append(jQuery("<li></li>").text(user));
-        }
-        jQuery("#users").html(ol);
-    }
-});
-
-socket.addEventListener("message", function(msg) {
-    const message = JSON.parse(msg.data);
-    if (message.text) {
-        const template = jQuery("#message-template").html();
-        const html = Mustache.render(template, {
-            text: message.text,
-            from: message.from,
-            createdAt: message.timeStamp,
-        });
-        jQuery("#message-list").append(html);
-        scrollToBottom();
-    }
+  const message = JSON.parse(msg.data);
+  if (message.text) {
+    const template = jQuery("#message-template").html();
+    const html = Mustache.render(template, {
+      text: message.text,
+      from: message.from,
+      createdAt: message.timeStamp,
+    });
+    jQuery("#message-list").append(html);
+    scrollToBottom();
+  }
 });
 
 jQuery("#message-form").on("submit", (e) => {
-    e.preventDefault();
-    const messageTextbox = jQuery("#message-form-message");
-    socket.send(
-        JSON.stringify({ type: "receivedMessage", content: messageTextbox.val() }),
-    );
-    messageTextbox.val("");
+  e.preventDefault();
+  const messageTextbox = jQuery("#message-form-message");
+  socket.send(messageTextbox.val());
+  messageTextbox.val("");
 });
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,57 @@
 mod users;
 use axum::{
+    debug_handler,
     extract::{
         ws::{Message, WebSocket},
-        State, WebSocketUpgrade,
+        Query, State, WebSocketUpgrade,
     },
     response::IntoResponse,
     routing::get,
     Router,
 };
-use axum_extra::{
-    headers::{self},
-    TypedHeader,
-};
+use axum_extra::{headers, TypedHeader};
 use core::panic;
 use futures::{sink::SinkExt, stream::StreamExt};
 use serde::{Deserialize, Serialize};
-use std::{
-    net::SocketAddr,
-    sync::{Arc, Mutex},
-};
+use std::{net::SocketAddr, sync::Arc};
+use tokio::sync::broadcast;
 use tower_http::{services::ServeDir, trace::TraceLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    let room = Arc::new(Mutex::new(Room::new()));
-
+    // let room = Arc::new(Mutex::new(Room::new()));
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_websockets=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
+    let broadcast_chan = Arc::new(broadcast::channel::<ChatMessage>(10));
     let app = Router::new()
         .fallback_service(ServeDir::new("public"))
         .route("/ws", get(ws_handler))
-        .with_state(room);
+        .with_state(broadcast_chan);
 
     axum::serve(listener, app.layer(TraceLayer::new_for_http()))
         .await
         .unwrap();
 }
 
+type RoomBroadcast = (
+    broadcast::Sender<ChatMessage>,
+    broadcast::Receiver<ChatMessage>,
+);
+
+#[debug_handler]
 async fn ws_handler(
-    State(room): State<Arc<Mutex<Room>>>,
+    State(room_broadcast): State<Arc<RoomBroadcast>>,
     ws: WebSocketUpgrade,
+    Query(user_info): Query<UserInfo>,
     user_agent: Option<TypedHeader<headers::UserAgent>>,
 ) -> impl IntoResponse {
     let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
@@ -51,72 +62,36 @@ async fn ws_handler(
     println!("`{user_agent}` ");
     // finalize the upgrade process by returning upgrade callback.
     // we can customize the callback by sending additional info such as address.
-    ws.on_upgrade(|socket| handle_socket(socket, room))
+    let sender = room_broadcast.0.clone();
+    let receiver = room_broadcast.0.subscribe();
+
+    ws.on_upgrade(|socket| handle_socket(socket, (sender, receiver), user_info))
 }
 
-async fn handle_socket(socket: WebSocket, room: Arc<Mutex<Room>>) {
-    let (user_send, mut user_recv) = tokio::sync::mpsc::unbounded_channel::<MyMessage>();
+async fn handle_socket(socket: WebSocket, broadcast: RoomBroadcast, user_info: UserInfo) {
+    let (bc_send, mut bc_recv) = broadcast;
     let (mut socket_send, mut socket_recv) = socket.split();
+
     tokio::spawn(async move {
-        while let Some(msg) = user_recv.recv().await {
-            match msg {
-                MyMessage::ReceivedMessage(msg) => {
-                    socket_send
-                        .send(Message::Text(
-                            serde_json::to_string(&ChatMessage {
-                                text: msg,
-                                from: String::from("test"),
-                            })
-                            .expect("Failed to serialize MyMessage"),
-                        ))
-                        .await
-                        .expect("Failed to send");
-                }
-                _ => todo!(),
-            }
+        while let Ok(msg) = bc_recv.recv().await {
+            let _ = socket_send
+                .send(Message::Text(
+                    serde_json::to_string(&msg).expect("Failed to serialize MyMessage"),
+                ))
+                .await;
         }
     });
 
-    let join_msg = socket_recv
-        .next()
-        .await
-        .expect("Should receive first message")
-        .expect("First message should deserialize");
-
-    let user_info: UserInfo = if let Message::Text(join_msg) = join_msg {
-        serde_json::from_str(&join_msg)
-            .expect("First message couldn't be deserialized into UserInfo")
-    } else {
-        panic!("First message isn't user info")
-    };
-    let user = User::new(user_send, user_info);
-
-    room.lock()
-        .expect("other thread panicked while holding lock")
-        .join_user(user);
     while let Some(msg) = socket_recv.next().await {
         println!("{:?}", msg);
         let msg = msg.expect("deserialize message");
         match msg {
             Message::Text(msg_txt) => {
-                println!("{:?}", &msg_txt);
-                match serde_json::from_str(&msg_txt).expect("deserialze MyMessage") {
-                    MyMessage::ReceivedMessage(new_msg_txt) => {
-                        println!("{:?}", new_msg_txt);
-                        if let Ok(mut room) = room.as_ref().lock() {
-                            room.send(&MyMessage::ReceivedMessage(new_msg_txt))
-                        }
-                    }
-                    MyMessage::UpdateUserList(s) => {
-                        println!("{:?}", s);
-                    }
-                    MyMessage::Unknown(msg) => {
-                        println!("{:?}", msg);
-                    }
-                    MyMessage::NewUser(_) => {
-                        panic!("User info changing after initialized");
-                    }
-                }
+                println!("{:?}", msg_txt);
+                let _ = bc_send.send(ChatMessage {
+                    text: msg_txt,
+                    from: String::from(&user_info.name),
+                });
             }
             Message::Close(_) => {}
             _ => {}
@@ -124,58 +99,17 @@ async fn handle_socket(socket: WebSocket, room: Arc<Mutex<Room>>) {
     }
 }
 
-struct Room {
-    users: Vec<User>,
-    messages: Vec<MyMessage>,
-}
-
-impl Room {
-    fn new() -> Self {
-        Self {
-            users: Default::default(),
-            messages: vec![],
-        }
-    }
-
-    fn join_user(&mut self, user: User) {
-        self.users.push(user);
-    }
-
-    fn send(&mut self, msg: &MyMessage) {
-        self.messages.push(msg.clone());
-        self.users.iter().for_each(|user| user.send_msg(msg));
-    }
-}
-
-struct User {
-    send_ch: tokio::sync::mpsc::UnboundedSender<MyMessage>,
-    user_info: UserInfo,
-}
-impl User {
-    fn new(chan: tokio::sync::mpsc::UnboundedSender<MyMessage>, user_info: UserInfo) -> Self {
-        Self {
-            send_ch: chan,
-            user_info,
-        }
-    }
-    fn send_msg(&self, msg: &MyMessage) {
-        self.send_ch.send(msg.clone()).expect("send failed");
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "type", rename_all = "camelCase", content = "content")]
-enum MyMessage {
-    ReceivedMessage(String),
-    NewUser(UserInfo),
-    UpdateUserList(Vec<String>),
-    Unknown(String),
-}
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct UserInfo {
     name: String,
     room: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content")]
+enum BroadcastMessage {
+    ChatMessage,
+    UpdateUserList,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
